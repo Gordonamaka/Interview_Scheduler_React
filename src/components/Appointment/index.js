1bd811fb0fead1111bec624d861d7355e2fc9f7c
Gordonamaka <amobimak@gmail.com>
2021-11-04 16:13:01 -0400

    adds Show/empty/cancel transition functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,26 +3,41 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 
 export default function Appointment(props) {
-  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
-    
-    <article className='appointment'>
+    <article className="appointment">
       <Fragment>
         <Header time={props.time}></Header>
-        {props.interview ?
           <>
-            <Show student={props.interview.student} interviewer={props.interview.interviewer.name}></Show>
+            {mode === EMPTY && (
+              <Empty onAdd={() => transition(CREATE)} />
+            )}
+            {mode === SHOW && (
+              <Show
+                student={props.interview.student}
+                interviewer={props.interview.interviewer.name}
+              />
+            )}
+            {mode === CREATE && (
+              <Form
+              interviewers={[]}
+              onCancel={() => back()}
+              />
+            )}
           </>
-          :
-          <>
-            <Empty></Empty>
-          </>
-        }
       </Fragment>
     </article>
-
-);
+  );
 }
