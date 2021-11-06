a93d6b0e30d4d67a2b64d0823b2a06295d93964b
Gordonamaka <amobimak@gmail.com>
2021-11-05 21:08:48 -0400

    adds appointment test

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+/*
+  We import our helper functions from the react-testing-library
+  The render function allows us to render Components
+*/
+import { render, cleanup } from "@testing-library/react";
+
+
+import Appointment from "components/Appointment/index"
+
+afterEach(cleanup)
+
+describe("Appointment", () => {
+
+it("render without crashing", () => {
+  render(<Appointment />);
+});
+
+});
